dd339b1f49bd16ce4c16ed83a14925da4a05552d
Linh Nguyen <linhvn09@gmail.com>
2022-01-18 16:43:33 +0700

    init node project with mongoose and express

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose";
+
+const app = express();
+const PORT = 8000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('It works')
+})
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
