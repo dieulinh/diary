fb573a551941c0c91dc0f5b18442571dc846bbc4
Linh Nguyen <linhvn09@gmail.com>
2022-02-17 10:39:15 +0700

    edit note

--- controllers/notes.js
+++ controllers/notes.js
@@ -22,6 +22,37 @@ const createNote = (req, res) => {
     })
   })
 };
+const updateNote = (req, res) => {
+  if (!req.body.title || !req.body.content) {
+    res.status(400).send({
+      message: 'Title or content cannot be blank'
+    });
+    return;
+  };
+  const id = req.params.id;
+  console.log(id);
+  const noteData = {title: req.body.title, content: req.body.content };
+  Note.findByIdAndUpdate(id, req.body, {useFindAndModify: false}).then((data) => {
+    res.status(200).send(data);
+  }).catch(err => {
+    res.status(500).send({message: `Error when updating note with id ${id}`});
+  })
+};
+const getNote = (req, res) => {
+  const id = req.params.id;
+  Note.findById(id)
+  .then((note) => {
+    if(!note) {
+      res.status(404).send({message: `Not found note with id: ${id}`})
+    } else {
+      res.status(200).send(note);
+      return
+    }
+
+  }).catch(err => {
+    res.status(500).send({message: `Error when fetching note with id ${id}`});
+  })
+}
 const getAllNotes = (req, res) => {
   User.findOne({username: req.params.username}).exec((err, user) => {
     if (err) {
@@ -38,5 +69,7 @@ const getAllNotes = (req, res) => {
 }
 export default {
   createNote,
+  getNote,
+  updateNote,
   getAllNotes
 }
--- routes/notes.js
+++ routes/notes.js
@@ -6,5 +6,7 @@ export default app => {
   const router = express.Router();
   router.post("/:username/add", notes.createNote);
   router.get("/:username/notes", notes.getAllNotes);
+  router.get("/note/:id", notes.getNote);
+  router.put("/note/:id", notes.updateNote);
   app.use(router);
 }
